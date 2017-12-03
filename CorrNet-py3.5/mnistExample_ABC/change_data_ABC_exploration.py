10ddc50c67b77c16c9579486fdaa288dd27c7e78
Dominik Lewy <dominik.lewy@lingaro.com>
2017-12-03 19:14:53 +0100

    [3pieces] adding original data source + measuring how much information is in left/right part of the image

--- CorrNet-py3.5/mnistExample_ABC/change_data_ABC_exploration.py
+++ CorrNet-py3.5/mnistExample_ABC/change_data_ABC_exploration.py
@@ -12,8 +12,8 @@ def get_mat(fname):
 	mat = np.asarray(mat,dtype="float32")
 	return mat
 
-left=get_mat("MNIST_DIR/test-view1.txt")
-right=get_mat("MNIST_DIR/test-view2.txt")
+left=get_mat("MNIST_DIR_original/test-view1.txt")
+right=get_mat("MNIST_DIR_original/test-view2.txt")
 
 plt.imshow(left[0].reshape(28,14), cmap='gray')
 plt.imshow(right[0].reshape(28,14), cmap='gray')
@@ -33,8 +33,21 @@ def split_image(matrix):
 A, B, C = split_image(full)
 
 plt.imshow(A[0], cmap='gray')
+plt.imshow(A[1], cmap='gray')
+plt.imshow(A[2], cmap='gray')
+plt.imshow(A[3], cmap='gray')
+plt.imshow(A[4], cmap='gray')
+plt.imshow(A[5], cmap='gray')
+plt.imshow(A[6], cmap='gray')
+plt.imshow(A[7], cmap='gray')
+plt.imshow(A[8], cmap='gray')
+
 plt.imshow(B[0], cmap='gray')
 plt.imshow(C[0], cmap='gray')
+
+A.sum()/B.sum()*100
+B.sum()
+C.sum()/B.sum()*100
 
 #how to save
 A_save=A.reshape(A.shape[0],252)
