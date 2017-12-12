7b0cfbf37951bd12d83fbf822f68e39314cf563e
Dominik Lewy <dominik.lewy@lingaro.com>
2017-12-12 12:02:01 +0100

    [master] adding loop to the image processing

--- CorrNet-py3.5/mnistExample_ABC/change_data_ABC_resize_exploration.py
+++ CorrNet-py3.5/mnistExample_ABC/change_data_ABC_resize_exploration.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import rotate
+import cv2
 
 def get_mat(fname):
 	file = open(fname,"r")
@@ -24,7 +25,7 @@ plt.imshow(test, cmap='gray')
 ##############WORKS
 
 #start
-img=full[1]
+img=full[4200]
 plt.imshow(img, cmap='gray')
 
 test=img>0
@@ -55,11 +56,13 @@ global_min=min((height_min,width_min))
 global_max
 global_min
 
-cut=min(27-height_max,width_min)-1
+cut=max(min(27-height_max,width_min),0)
+cut2=max(min(27-width_max,height_min),0)
 cut
+cut2
 
 plt.imshow(img, cmap='gray')
-img_cropped=img[global_min:global_max-cut,cut:global_max]
+img_cropped=img[cut2:global_max-cut,cut:global_max-cut2]
 plt.imshow(img_cropped, cmap='gray')
 img_cropped_resized=scipy.misc.imresize(img_cropped,[28,28])
 plt.imshow(img_cropped_resized, cmap='gray')
@@ -72,3 +75,46 @@ im=Image.open("test.png")
 im
 im.resize([500,500])
 
+# for loop to iterate through full data set
+full_transformed=np.ndarray([10000,28,28])
+
+for i in range(0, 10):
+    img=full[i]
+    test=img>0
+    width=test.max(axis=0)
+    height=test.max(axis=1)
+#idicators if there is information in horizontal and vertical lines
+    height=np.where(height==True)
+    height_min=height[0].min()
+    height_max=height[0].max()
+    
+    width=np.where(width==True)
+    width_min=width[0].min()
+    width_max=width[0].max()
+#idicators if there is information in horizontal and vertical lines
+
+    global_max=max((height_max,width_max))+1
+    global_min=min((height_min,width_min))
+
+    cut=max(min(27-height_max,width_min),0)
+    cut2=max(min(27-width_max,height_min),0)
+    
+    img=scipy.misc.imresize(img,[28,28])
+    img_cropped=img[cut2:global_max-cut,cut:global_max-cut2]
+    img_cropped_resized=scipy.misc.imresize(img_cropped,[28,28])
+    full_transformed[i]=img_cropped_resized
+    #monitoring progress
+    if i%1==0:
+        print(i)
+    #checking results
+    if i<=10:
+        im_original = Image.fromarray(img).convert("RGB")
+        im_transformed = Image.fromarray(img_cropped_resized).convert("RGB")
+        name_original="plots/image_"+str(i)+".png"
+        name_transformed="plots/image_"+str(i)+"_transformed.png"
+        im_original.save(name_original)
+        im_transformed.save(name_transformed)
+        
+
+plt.imshow(full[4200], cmap='gray')
+plt.imshow(full_transformed[6], cmap='gray')
